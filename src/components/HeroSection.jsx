fb8082278e7ab610829c6b156c3fed1c77ac8a71
mhidusti <mhidusti@gmail.com>
2025-10-14 13:00:40 +0330

    add css

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,121 +1,135 @@
-import React from 'react';
+import React from "react";
 
-export default function HeroSection() {
+const HeroSection = () => {
   return (
-    <>
-      <section className="hero-section style-2 d-flex align-items-center" id="intro">
+    <main className="site-content" id="content">
+      {/* HERO SECTION START */}
+      <section className="hero-section d-flex align-items-center" id="intro">
+        <div className="intro_text">
+          <svg viewBox="0 0 1320 300">
+            <text x="50%" y="50%" textAnchor="middle">
+              HI
+            </text>
+          </svg>
+        </div>
+
         <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="hero-wrrapper wow fadeInUp" data-wow-delay=".4s">
-                <div className="hero-image-box d-none d-md-inline-block">
-                  <img src="/assets/img/hero/hero-2.png" alt="Hero background" />
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <div className="hero-content-box">
+                <span className="hero-sub-title">I am Gerold</span>
+                <h1 className="hero-title">
+                  Web Developer +<br />UX Designer
+                </h1>
+
+                <div className="hero-image-box d-md-none text-center">
+                  <img src="assets/img/hero/me.png" alt="" />
                 </div>
 
-                <div className="hero-content-box">
-                  <h1 className="hero-title">
-                    سلام، من سام هستم - وب <br /> توسعه دهنده + طراح UX
-                  </h1>
+                <p className="lead">
+                  I break down complex user experinece problems to create
+                  integritiy focussed solutions that connect billions of people
+                </p>
 
-                  <div className="hero-image-box d-md-none text-center">
-                    <img src="/assets/img/hero/me.png" alt="Sam portrait" />
+                <div className="button-box d-flex flex-wrap align-items-center">
+                  <a href="#" className="btn tj-btn-secondary">
+                    Download CV <i className="flaticon-download"></i>
+                  </a>
+                  <ul className="ul-reset social-icons">
+                    <li>
+                      <a href="#">
+                        <i className="fa-brands fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="fa-light fa-basketball"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="fa-brands fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i className="fa-brands fa-github"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 d-none d-md-block">
+              <div className="hero-image-box text-center">
+                <img src="assets/img/hero/me.png" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className="funfact-area">
+            <div className="row">
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number">
+                    <span className="odometer" data-count="14">
+                      0
+                    </span>
                   </div>
-
-                  <p className="lead">
-                    من چیزهای ساده و زیبا را طراحی و کدنویسی می‌کنم و کاری را که انجام می‌دهم دوست دارم.
-                  </p>
-
-                  <div className="button-box d-flex flex-wrap align-items-center">
-                    <a href="#" className="tj-btn-primary-2">
-                      دریافت رزومه !
-                      <span className="icon_box" aria-hidden="true">
-                        <i className="fa-regular fa-arrow-right icon_first"></i>
-                        <i className="fa-regular fa-arrow-right icon_second"></i>
-                      </span>
-                    </a>
-
-                    <ul className="ul-reset social-icons style-2">
-                      <li><a href="#"><i className="fa-brands fa-twitter" aria-hidden="true"></i></a></li>
-                      <li><a href="#"><i className="fa-light fa-basketball" aria-hidden="true"></i></a></li>
-                      <li><a href="#"><i className="fa-brands fa-linkedin-in" aria-hidden="true"></i></a></li>
-                      <li><a href="#"><i className="fa-brands fa-github" aria-hidden="true"></i></a></li>
-                    </ul>
+                  <div className="text">
+                    Years of <br />Experience
                   </div>
                 </div>
               </div>
 
-              <div className="funfact-area style-2">
-                <div className="funfact-item wow fadeInUp" data-wow-delay=".3s">
-                  <div className="funfact-icon">
-                    <i className="flaticon-trophy"></i>
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number">
+                    <span className="odometer" data-count="50">
+                      0
+                    </span>
+                    +
                   </div>
-                  <div className="number"><span className="odometer" data-count="14">0</span>%</div>
-                  <div className="text">دستاوردهای شغلی</div>
-                </div>
-
-                <div className="funfact-item wow fadeInUp" data-wow-delay=".4s">
-                  <div className="funfact-icon">
-                    <i className="flaticon-suitcase"></i>
-                  </div>
-                  <div className="number"><span className="odometer" data-count="50">0</span>+</div>
-                  <div className="text">سابقه کاری</div>
-                </div>
-
-                <div className="funfact-item wow fadeInUp" data-wow-delay=".5s">
-                  <div className="funfact-icon">
-                    <i className="flaticon-menu"></i>
-                  </div>
-                  <div className="number"><span className="odometer" data-count="1.5">0</span>K</div>
-                  <div className="text">مشتریان خوشحال</div>
-                </div>
-
-                <div className="funfact-item wow fadeInUp" data-wow-delay=".6s">
-                  <div className="funfact-icon">
-                    <i className="flaticon-crown"></i>
-                  </div>
-                  <div className="number"><span className="odometer" data-count="14">0</span>+</div>
-                  <div className="text">پروژه‌های اتمام شده</div>
-                </div>
-              </div>
-
-              <div className="about-area">
-                <div className="row">
-                  <div className="col-lg-7">
-                    <div className="about-left-content wow fadeInUp" data-wow-delay=".3s">
-                      <h2 className="title">دستاوردهای زندگی حرفه‌ای من.</h2>
-                      <div className="desc">
-                        <p className="lead">لورم ایپسوم متن ساختگی ...</p>
-                      </div>
-                      <div className="about-button d-flex">
-                        <a href="#" className="btn tj-btn-primary-2">
-                          تماس با من
-                          <span className="icon_box" aria-hidden="true">
-                            <i className="fa-regular fa-arrow-right icon_first"></i>
-                            <i className="fa-regular fa-arrow-right icon_second"></i>
-                          </span>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col-lg-5">
-                    <div className="about-right-content wow fadeInUp" data-wow-delay=".5s">
-                      <div className="about-text">
-                        <h5 className="title">طراح رابط</h5>
-                        <div className="desc"><p>لورم ایپسوم ...</p></div>
-                      </div>
-                      <div className="about-images">
-                        <img src="/assets/img/hero/about-1.png" alt="About image" />
-                      </div>
-                    </div>
+                  <div className="text">
+                    Project <br />Completed
                   </div>
                 </div>
               </div>
 
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number">
+                    <span className="odometer" data-count="1.5">
+                      0
+                    </span>
+                    K
+                  </div>
+                  <div className="text">
+                    Happy <br />Clients
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-6 col-lg-3">
+                <div className="funfact-item d-flex flex-column flex-sm-row flex-wrap align-items-center">
+                  <div className="number">
+                    <span className="odometer" data-count="14">
+                      0
+                    </span>
+                  </div>
+                  <div className="text">
+                    Years of <br />Experience
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
-    </>
+      {/* HERO SECTION END */}
+    </main>
   );
-}
+};
+
+export default HeroSection;
